2391abdf76c0bfd5b4a1b8aeb4be1c6f9726c772
Djalma Araujo <djalma.araujo@gmail.com>
2016-04-13 22:22:45 -0300

    port update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(5555);
+server.listen(80);
 
 app.get('/', function (req, res) {
   res.send(200);
